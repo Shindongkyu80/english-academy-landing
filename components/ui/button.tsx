13617c938de0ddc55d5f0973deda4002b3ccb13e
sdk180531 <sdk180531@gmail.com>
2025-02-12 00:28:45 +0900

    충돌 해결

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,15 +1,22 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'default' | 'sm' | 'lg' | 'icon'
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, size = 'default', ...props }, ref) => {
     return (
       <button
         className={cn(
           "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+          {
+            'h-10 px-4 py-2': size === 'default',
+            'h-9 px-3': size === 'sm',
+            'h-12 px-8 text-lg': size === 'lg',
+            'h-10 w-10': size === 'icon',
+          },
           className
         )}
         ref={ref}
